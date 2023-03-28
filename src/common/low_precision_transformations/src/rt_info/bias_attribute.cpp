7a5ba3fdcd2b6c0c76c60e17c30b142ef70bf0e5
Vladislav Golubev <vladislav.golubev@intel.com>
2023-03-28 11:12:33 +0200

    is_copyable fix

--- src/common/low_precision_transformations/src/rt_info/bias_attribute.cpp
+++ src/common/low_precision_transformations/src/rt_info/bias_attribute.cpp
@@ -3,11 +3,13 @@
 //
 
 #include "low_precision/rt_info/bias_attribute.hpp"
+#include "low_precision/network_helper.hpp"
 
+#include <iterator>
 #include <memory>
+#include <openvino/opsets/opset1.hpp>
 #include <string>
 #include <unordered_map>
-#include <iterator>
 #include <vector>
 
 void ov::mark_as_bias(const std::shared_ptr<ov::Node>& node) {
@@ -18,4 +20,8 @@ void ov::mark_as_bias(const std::shared_ptr<ov::Node>& node) {
 bool ov::marked_as_bias(const std::shared_ptr<const ov::Node>& node) {
     const auto& rt_info = node->get_rt_info();
     return rt_info.find(ov::BiasAttribute::get_type_info_static()) != rt_info.end();
+}
+
+bool ov::BiasAttribute::is_copyable(const std::shared_ptr<ov::Node>& to) const {
+    return ov::is_type<ov::opset1::Add>(to) && ngraph::pass::low_precision::NetworkHelper::getConstantInput(to) != nullptr;
 }

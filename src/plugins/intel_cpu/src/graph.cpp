d1a1d4fa90aa0745a9a401536ffbd355cc6ec1b6
xuchen-intel <chen.xu@intel.com>
2023-05-05 21:01:01 -0700

    Get original input precision fp16

--- src/plugins/intel_cpu/src/graph.cpp
+++ src/plugins/intel_cpu/src/graph.cpp
@@ -329,6 +329,7 @@ void Graph::Replicate(const CNNNetwork &network) {
         for (size_t i = 0; i < childEdges.size(); i++) {
             const auto child = childEdges[i]->getChild();
             if (child->getOriginalInputPrecisionAtPort(childEdges[i]->getOutputNum()) != Precision::BF16 &&
+                child->getOriginalInputPrecisionAtPort(childEdges[i]->getOutputNum()) != Precision::FP16 &&
                 // remove this WA when #78939 is resolved
                 !hasSubgraphConsumers(child))
                 child->setOriginalInputPrecisionAtPort(childEdges[i]->getOutputNum(), precToSet);
@@ -1604,6 +1605,7 @@ void Graph::EnforceInferencePrecision() {
                                                                          Type::Subgraph,
                                                                          Type::MVN,
                                                                          Type::Softmax,
+                                                                         Type::Reduce,
                                                                          Type::Reshape,
                                                                          Type::Gather,
                                                                          Type::Split))

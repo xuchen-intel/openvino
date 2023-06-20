8ce4a4d5975b08913f1e1582de823948405398b8
xuchen-intel <chen.xu@intel.com>
2023-06-20 22:59:58 +0800

    [WIP] Snippets loop unrolling

---
+++ src/common/snippets/src/lowered/pass/unroll_loops.cpp
@@ -0,0 +1,81 @@
+// Copyright (C) 2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "snippets/lowered/pass/unroll_loops.hpp"
+
+#include "snippets/lowered/linear_ir.hpp"
+#include "snippets/snippets_isa.hpp"
+#include "snippets/itt.hpp"
+
+namespace ov {
+namespace snippets {
+namespace lowered {
+namespace pass {
+
+bool UnrollLoops::run(LinearIR& linear_ir) {
+    OV_ITT_SCOPED_TASK(ov::pass::itt::domains::SnippetsTransform, "Snippets::UnrollLoops")
+    bool modified = false;
+
+    auto is_supported_eltwise_node = [](const std::shared_ptr<ov::Node>& node) {
+        if (ov::is_type<const ov::op::v1::Maximum>(node) ||
+            ov::is_type<const ov::op::v1::Subtract>(node) ||
+            ov::is_type<const ov::op::v0::Exp>(node) ||
+            ov::is_type<const ov::op::v1::Add>(node) ||
+            ov::is_type<const ov::op::v1::Multiply>(node)) {
+            return true;
+        }
+        return false;
+    };
+
+    std::set<size_t> unroll_work_step;
+    for (auto expr_it = linear_ir.begin(); expr_it != linear_ir.end();) {
+        const auto& loop_begin = ov::as_type_ptr<ov::snippets::op::LoopBegin>((*expr_it)->get_node());
+        if (!loop_begin) {
+            expr_it++;
+            continue;
+        }
+        const auto& loop_end = loop_begin->get_loop_end();
+
+        // Ignore outer loops and tail loops
+        if (loop_end->get_increment() < loop_end->get_work_amount()) {
+            auto& loop_expr_it = expr_it;
+            loop_expr_it++;
+            bool is_supported = true;
+            while ((*loop_expr_it)->get_node() != loop_end) {
+                const auto& node = (*loop_expr_it)->get_node();
+                if (ov::is_type<const snippets::op::MemoryAccess>(node)) {
+                    loop_expr_it++;
+                    continue;
+                }
+                if (!is_supported_eltwise_node(node)) {
+                    is_supported = false;
+                    break;
+                }
+                loop_expr_it++;
+            }
+
+            if (is_supported) {
+                loop_end->set_unroll_loop(true);
+                unroll_work_step.insert(loop_end->get_increment());
+                modified = true;
+            }
+        } else {
+            expr_it++;
+        }
+    }
+
+    if (modified && unroll_work_step.size() == 1) {
+        linear_ir.set_unroll_loop(true);
+        linear_ir.set_unroll_work_step(*unroll_work_step.begin());
+        return true;
+    }
+
+    linear_ir.set_unroll_loop(false);
+    return false;
+}
+
+} // namespace pass
+} // namespace lowered
+} // namespace snippets
+} // namespace ov

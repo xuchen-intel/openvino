e1ff15b074bb2b0878afe0dfe8e18965b76f8b9b
xuchen-intel <chen.xu@intel.com>
2023-06-20 23:05:18 +0800

    Apply OV_ITT_SCOPED_TASK

--- src/common/snippets/src/lowered/pass/loop_unrolling.cpp
+++ src/common/snippets/src/lowered/pass/loop_unrolling.cpp
@@ -5,6 +5,7 @@
 #include "snippets/lowered/pass/loop_unrolling.hpp"
 
 #include "snippets/lowered/linear_ir.hpp"
+#include "snippets/itt.hpp"
 
 namespace ov {
 namespace snippets {
@@ -12,6 +13,8 @@ namespace lowered {
 namespace pass {
 
 bool LoopUnrolling::run(LinearIR& linear_ir) {
+    OV_ITT_SCOPED_TASK(ov::pass::itt::domains::SnippetsTransform, "Snippets::LoopUnrolling")
+
     return true;
 }
 

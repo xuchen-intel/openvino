b49def4125e40d869e9636963911b39e85be5c89
xuchen-intel <chen.xu@intel.com>
2023-12-18 04:36:46 +0000

    Add NopEmitter and jit_add_emitter

--- src/plugins/intel_cpu/src/emitters/aarch64/cpu_generator.cpp
+++ src/plugins/intel_cpu/src/emitters/aarch64/cpu_generator.cpp
@@ -4,8 +4,30 @@
 
 #include "snippets/snippets_isa.hpp"
 #include "cpu_generator.hpp"
+#include "jit_snippets_emitters.hpp"
+#include "jit_eltwise_emitters.hpp"
+
+#include <openvino/opsets/opset13.hpp>
 
 namespace ov {
+
+#define CREATE_SNIPPETS_EMITTER(e_type) { \
+    [this](const snippets::lowered::ExpressionPtr& expr) -> std::shared_ptr<snippets::Emitter> { \
+        return std::make_shared<e_type>(h.get(), isa, expr); \
+    }, \
+    [](const std::shared_ptr<ov::Node>& n) -> std::set<std::vector<element::Type>> { \
+        return e_type::get_supported_precisions(n); \
+    } \
+}
+
+#define CREATE_CPU_EMITTER(e_type) { \
+    [this](const snippets::lowered::ExpressionPtr& expr) -> std::shared_ptr<snippets::Emitter> { \
+        return std::make_shared<e_type>(h.get(), isa, expr->get_node()); \
+    }, \
+    [](const std::shared_ptr<ov::Node>& n) -> std::set<std::vector<element::Type>> { \
+        return e_type::get_supported_precisions(n); \
+    } \
+}
 
 class jit_snippet : public dnnl::impl::cpu::aarch64::jit_generator {
 public:
@@ -18,30 +40,41 @@ public:
     void generate() override {}
 };
 
-intel_cpu::CompiledSnippetCPU::CompiledSnippetCPU(std::unique_ptr<dnnl::impl::cpu::aarch64::jit_generator> h) : h_compiled(std::move(h)) {
+namespace intel_cpu {
+namespace aarch64 {
+
+CompiledSnippetCPU::CompiledSnippetCPU(std::unique_ptr<dnnl::impl::cpu::aarch64::jit_generator> h) : h_compiled(std::move(h)) {
     OPENVINO_ASSERT(h_compiled && h_compiled->jit_ker(), "Got invalid jit generator or kernel was nopt compiled");
 }
 
-const uint8_t* intel_cpu::CompiledSnippetCPU::get_code() const {
+const uint8_t* CompiledSnippetCPU::get_code() const {
     return h_compiled->jit_ker();
 }
 
-size_t intel_cpu::CompiledSnippetCPU::get_code_size() const {
+size_t CompiledSnippetCPU::get_code_size() const {
     return h_compiled->getSize();
 }
 
-bool intel_cpu::CompiledSnippetCPU::empty() const {
+bool CompiledSnippetCPU::empty() const {
     return get_code_size() == 0;
 }
 
-intel_cpu::CPUTargetMachine::CPUTargetMachine(dnnl::impl::cpu::aarch64::cpu_isa_t host_isa)
-    : TargetMachine(), h(new jit_snippet()), isa(host_isa) {}
+CPUTargetMachine::CPUTargetMachine(dnnl::impl::cpu::aarch64::cpu_isa_t host_isa)
+    : TargetMachine(), h(new jit_snippet()), isa(host_isa) {
+    // todo: add implementation
+    // data movement
+    jitters[op::v0::Parameter::get_type_info_static()] = CREATE_SNIPPETS_EMITTER(NopEmitter);
+    jitters[op::v0::Result::get_type_info_static()] = CREATE_SNIPPETS_EMITTER(NopEmitter);
 
-bool intel_cpu::CPUTargetMachine::is_supported() const {
+    // binary
+    jitters[op::v1::Add::get_type_info_static()] = CREATE_CPU_EMITTER(jit_add_emitter);
+}
+
+bool CPUTargetMachine::is_supported() const {
     return dnnl::impl::cpu::aarch64::mayiuse(isa);
 }
 
-snippets::CompiledSnippetPtr intel_cpu::CPUTargetMachine::get_snippet() {
+snippets::CompiledSnippetPtr CPUTargetMachine::get_snippet() {
     if (h->create_kernel() != dnnl::impl::status::success) {
         OPENVINO_THROW("Failed to create jit_kernel in get_snippet()");
     }
@@ -51,34 +84,36 @@ snippets::CompiledSnippetPtr intel_cpu::CPUTargetMachine::get_snippet() {
     return result;
 }
 
-size_t intel_cpu::CPUTargetMachine::get_lanes() const {
+size_t CPUTargetMachine::get_lanes() const {
     switch (isa) {
         case dnnl::impl::cpu::aarch64::asimd : return dnnl::impl::cpu::aarch64::cpu_isa_traits<dnnl::impl::cpu::aarch64::asimd>::vlen / sizeof(float);
         default : OPENVINO_THROW("unknown isa ", isa);
     }
 }
 
-dnnl::impl::cpu::aarch64::cpu_isa_t intel_cpu::CPUTargetMachine::get_isa() const {
+dnnl::impl::cpu::aarch64::cpu_isa_t CPUTargetMachine::get_isa() const {
     return isa;
 }
 
-intel_cpu::CPUGenerator::CPUGenerator(dnnl::impl::cpu::aarch64::cpu_isa_t isa_) : Generator(std::make_shared<intel_cpu::CPUTargetMachine>(isa_)) {}
+CPUGenerator::CPUGenerator(dnnl::impl::cpu::aarch64::cpu_isa_t isa_) : Generator(std::make_shared<CPUTargetMachine>(isa_)) {}
 
-std::shared_ptr<snippets::Generator> intel_cpu::CPUGenerator::clone() const {
+std::shared_ptr<snippets::Generator> CPUGenerator::clone() const {
     const auto& cpu_target_machine = std::dynamic_pointer_cast<CPUTargetMachine>(target);
     OPENVINO_ASSERT(cpu_target_machine, "Failed to clone CPUGenerator: the instance contains incompatible TargetMachine type");
     return std::make_shared<CPUGenerator>(cpu_target_machine->get_isa());
 }
 
-snippets::Generator::opRegType intel_cpu::CPUGenerator::get_specific_op_reg_type(const std::shared_ptr<ov::Node>& op) const {
+snippets::Generator::opRegType CPUGenerator::get_specific_op_reg_type(const std::shared_ptr<ov::Node>& op) const {
     // todo: add implementation
     OPENVINO_THROW("Register type of the operation " + std::string(op->get_type_name()) + " isn't determined!");
     return gpr2gpr;
 }
 
-bool intel_cpu::CPUGenerator::uses_precompiled_kernel(const std::shared_ptr<snippets::Emitter>& e) const {
+bool CPUGenerator::uses_precompiled_kernel(const std::shared_ptr<snippets::Emitter>& e) const {
     // todo: add implementation
     return false;
 }
 
-} // namespace ov
+}   // namespace aarch64
+}   // namespace intel_cpu
+}   // namespace ov

61b7ad916b1d6536346266e00cf79f964268829d
xuchen-intel <chen.xu@intel.com>
2023-12-19 08:30:08 +0000

    Add LoopBeginEmitter

--- src/plugins/intel_cpu/src/emitters/aarch64/jit_snippets_emitters.hpp
+++ src/plugins/intel_cpu/src/emitters/aarch64/jit_snippets_emitters.hpp
@@ -35,6 +35,30 @@ private:
                    const std::vector<size_t>& out) const override {}
 };
 
+class LoopBeginEmitter : public jit_emitter {
+public:
+    LoopBeginEmitter(dnnl::impl::cpu::aarch64::jit_generator* h,
+                     dnnl::impl::cpu::aarch64::cpu_isa_t isa,
+                     const ov::snippets::lowered::ExpressionPtr& expr);
+    void emit_code(const std::vector<size_t> &in,
+                   const std::vector<size_t> &out) const;
+    size_t get_inputs_count() const override {return 0;}
+
+private:
+    using jit_emitter::emit_code;
+    void validate_arguments(const std::vector<size_t> &in,
+                            const std::vector<size_t> &out) const override;
+    void emit_impl(const std::vector<size_t>& in,
+                   const std::vector<size_t>& out) const override;
+    template <dnnl::impl::cpu::aarch64::cpu_isa_t isa>
+    void emit_isa(const std::vector<size_t>& in,
+                   const std::vector<size_t>& out) const;
+
+    std::shared_ptr<snippets::op::LoopBegin> loop_begin;
+    bool evaluate_once = false;
+    size_t work_amount = 0;
+};
+
 }   // namespace aarch64
 }   // namespace intel_cpu
 }   // namespace ov

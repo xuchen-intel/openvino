7ed4827657acf436c187f0b9bb49a3adc7199a7d
xuchen-intel <chen.xu@intel.com>
2024-01-11 02:27:04 +0000

    jit_load_emitter and jit_store_emitter support tail

--- src/plugins/intel_cpu/src/emitters/aarch64/jit_load_store_emitters.cpp
+++ src/plugins/intel_cpu/src/emitters/aarch64/jit_load_store_emitters.cpp
@@ -42,11 +42,42 @@ void jit_load_emitter::emit_isa(const std::vector<size_t> &in_idxs, const std::v
     if (load_num_ > static_cast<int>((get_vec_length() / dst_prc_.size())))
         OPENVINO_THROW("Load emitter in ", name_, " have unexpected number of elements to load.");
 
-    using TReg = typename dnnl::impl::cpu::aarch64::cpu_isa_traits<isa>::TReg;
-    XReg xreg = XReg(convert_to_u32<size_t>(in_idxs)[0]);
-    TReg treg = TReg(convert_to_u32<size_t>(out_idxs)[0]);
+    auto in_idxs_u32 = convert_to_u32<size_t>(in_idxs);
+    auto out_idxs_u32 = convert_to_u32<size_t>(out_idxs);
 
-    h->uni_ldr(treg, xreg, byte_offset_);
+    using TReg = typename dnnl::impl::cpu::aarch64::cpu_isa_traits<isa>::TReg;
+    XReg src = XReg(in_idxs_u32[0]);
+    XReg prc = XReg(aux_gpr_idxs[0]);
+    TReg dst = TReg(out_idxs_u32[0]);
+    SReg dst_s = SReg(out_idxs_u32[0]);
+    DReg dst_d = DReg(out_idxs_u32[0]);
+
+    h->add(prc, src, byte_offset_);
+
+    switch (load_num_) {
+        case 0:
+            break;
+        case 1:
+            h->ldr(dst_s, ptr(prc));
+            break;
+        case 2:
+            h->ldr(dst_d, ptr(prc));
+            break;
+        case 3:
+            h->ldr(dst_d, ptr(prc));
+            h->add(prc, prc, 2 * sizeof(float));
+            h->ld1(dst.s[2], ptr(prc));
+            break;
+        case 4:
+            h->uni_ldr(dst, prc);
+            break;
+        default:
+            OPENVINO_THROW("Load emitter in ", name_, " has unexpected number of elements to load.");
+    }
+}
+
+size_t jit_load_emitter::get_aux_gprs_count() const {
+    return 1;
 }
 
 jit_store_emitter::jit_store_emitter(dnnl::impl::cpu::aarch64::jit_generator *host, dnnl::impl::cpu::aarch64::cpu_isa_t host_isa,
@@ -75,11 +106,42 @@ void jit_store_emitter::emit_isa(const std::vector<size_t> &in_idxs, const std::
     if (store_num_ > static_cast<int>((get_vec_length() / dst_prc_.size())))
         OPENVINO_THROW("Store emitter in ", name_, " have unexpected number of elements to store.");
 
-    using TReg = typename dnnl::impl::cpu::aarch64::cpu_isa_traits<isa>::TReg;
-    TReg treg = TReg(convert_to_u32<size_t>(in_idxs)[0]);
-    XReg xreg = XReg(convert_to_u32<size_t>(out_idxs)[0]);
+    auto in_idxs_u32 = convert_to_u32<size_t>(in_idxs);
+    auto out_idxs_u32 = convert_to_u32<size_t>(out_idxs);
 
-    h->str(QReg(treg.getIdx()), ptr(xreg, byte_offset_));
+    using TReg = typename dnnl::impl::cpu::aarch64::cpu_isa_traits<isa>::TReg;
+    TReg src = TReg(in_idxs_u32[0]);
+    SReg src_s = SReg(in_idxs_u32[0]);
+    DReg src_d = DReg(in_idxs_u32[0]);
+    XReg dst = XReg(out_idxs_u32[0]);
+    XReg prc = XReg(aux_gpr_idxs[0]);
+
+    h->add(prc, dst, byte_offset_);
+
+    switch (store_num_) {
+        case 0:
+            break;
+        case 1:
+            h->str(src_s, ptr(prc));
+            break;
+        case 2:
+            h->str(src_d, ptr(prc));
+            break;
+        case 3:
+            h->str(src_d, ptr(prc));
+            h->add(prc, prc, 2 * sizeof(float));
+            h->st1(src.s[2], ptr(prc));
+            break;
+        case 4:
+            h->str(QReg(src.getIdx()), ptr(prc));
+            break;
+        default:
+            OPENVINO_THROW("Store emitter in ", name_, " has unexpected number of elements to store.");
+    }
+}
+
+size_t jit_store_emitter::get_aux_gprs_count() const {
+    return 1;
 }
 
 }   // namespace aarch64

cf03c83e8398af438488bdb6fcf77b7d560dac39
xuchen-intel <chen.xu@intel.com>
2024-03-29 04:28:02 +0000

    Fix issue about reg_count

--- src/common/snippets/src/generator.cpp
+++ src/common/snippets/src/generator.cpp
@@ -34,7 +34,7 @@ void Generator::generate(lowered::LinearIR& linear_ir, LoweringResult& result, c
     //       (this might happen if tail loop and main loop have different increments)
     //    3. OptimizeLoopSingleEvaluation must be called after CleanupLoopOffsets
     //       since CleanupLoopOffsets can't handle loops with evaluate_once = true
-    lowered_pipeline.register_pass<lowered::pass::AssignRegisters>(reg_type_mapper);
+    lowered_pipeline.register_pass<lowered::pass::AssignRegisters>(reg_type_mapper, get_target_machine()->get_reg_count());
     lowered_pipeline.register_pass<lowered::pass::InsertSpecificIterations>();
     lowered_pipeline.register_pass<lowered::pass::CleanupLoopOffsets>();
     lowered_pipeline.register_pass<lowered::pass::OptimizeLoopSingleEvaluation>();

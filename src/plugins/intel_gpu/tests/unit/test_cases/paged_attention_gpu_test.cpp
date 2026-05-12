fa7982c7d841539c03423205836ea4f243abde2c
xuchen-intel <chen.xu@intel.com>
2026-05-12 22:40:30 +0800

    revert

--- src/plugins/intel_gpu/tests/unit/test_cases/paged_attention_gpu_test.cpp
+++ src/plugins/intel_gpu/tests/unit/test_cases/paged_attention_gpu_test.cpp
@@ -3032,21 +3032,6 @@ INSTANTIATE_TEST_SUITE_P(smoke_cm_xattention_head_size, xattention_test, ::testi
     paged_attention_test_params{ {{1, 1023}}, 2, 2, 256, 256, 256, 0, ENABLE_CACHE_COMPRESSION, ov::internal::CacheQuantMode::BY_CHANNEL, STATIC_INPUT_PAD, DISABLE_SCORES, DISABLE_ROTATION, DISABLE_FA_V2, false, 0, {}, true, std::vector<float>{100.0f}, std::vector<int>{128} },
 }));
 
-// Mode 2: Legacy branch with head_size=256, testing head_size partitioning optimization
-INSTANTIATE_TEST_SUITE_P(smoke_cm_xattention_head_size_developing, xattention_test, ::testing::ValuesIn(std::vector<paged_attention_test_params>{
-    // F16 cache - prefill and generate
-    paged_attention_test_params{ {{32, 0}},   4, 2, 256, 256, 256, 0, DISABLE_CACHE_COMPRESSION, ov::internal::CacheQuantMode::BY_TOKEN, STATIC_INPUT_PAD, DISABLE_SCORES, DISABLE_ROTATION, ENABLE_FA_V2, false, 0, {}, true, std::vector<float>{100.0f}, std::vector<int>{128} },
-    paged_attention_test_params{ {{1, 31}},   2, 2, 256, 256, 256, 0, DISABLE_CACHE_COMPRESSION, ov::internal::CacheQuantMode::BY_TOKEN, STATIC_INPUT_PAD, DISABLE_SCORES, DISABLE_ROTATION, DISABLE_FA_V2, false, 0, {}, true, std::vector<float>{100.0f}, std::vector<int>{128} },
-
-    // I8 BY_TOKEN cache - prefill and generate
-    paged_attention_test_params{ {{32, 0}},   2, 2, 256, 256, 256, 0, ENABLE_CACHE_COMPRESSION, ov::internal::CacheQuantMode::BY_TOKEN, STATIC_INPUT_PAD, DISABLE_SCORES, DISABLE_ROTATION, ENABLE_FA_V2, false, 0, {}, true, std::vector<float>{100.0f}, std::vector<int>{128} },
-    paged_attention_test_params{ {{1, 31}},   2, 2, 256, 256, 256, 0, ENABLE_CACHE_COMPRESSION, ov::internal::CacheQuantMode::BY_TOKEN, STATIC_INPUT_PAD, DISABLE_SCORES, DISABLE_ROTATION, DISABLE_FA_V2, false, 0, {}, true, std::vector<float>{100.0f}, std::vector<int>{128} },
-
-    // I8 BY_CHANNEL cache - prefill and generate
-    paged_attention_test_params{ {{32, 0}},   4, 2, 256, 256, 256, 0, ENABLE_CACHE_COMPRESSION, ov::internal::CacheQuantMode::BY_CHANNEL, STATIC_INPUT_PAD, DISABLE_SCORES, DISABLE_ROTATION, ENABLE_FA_V2, false, 0, {}, true, std::vector<float>{100.0f}, std::vector<int>{128} },
-    paged_attention_test_params{ {{1, 31}},   2, 2, 256, 256, 256, 0, ENABLE_CACHE_COMPRESSION, ov::internal::CacheQuantMode::BY_CHANNEL, STATIC_INPUT_PAD, DISABLE_SCORES, DISABLE_ROTATION, DISABLE_FA_V2, false, 0, {}, true, std::vector<float>{100.0f}, std::vector<int>{128} },
-}));
-
 INSTANTIATE_TEST_SUITE_P(smoke_cm_xattention_block_size, xattention_test, ::testing::ValuesIn(std::vector<paged_attention_test_params>{
     // Valid values
     paged_attention_test_params{ {{1024, 0}}, 2, 2, 64, 64, 256, 0, DISABLE_CACHE_COMPRESSION, ov::internal::CacheQuantMode::BY_TOKEN, STATIC_INPUT_PAD, DISABLE_SCORES, DISABLE_ROTATION, ENABLE_FA_V2, false, 0, {}, true, std::vector<float>{0.9f}, std::vector<int>{128} },
